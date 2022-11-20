99bd7d3badbdda9c5e16dc1b8687af17def7981a
Akash Garg <akgarg023@gmail.com>
2022-11-20 18:13:46 +0530

    file path from args

--- src/main/java/com/ledger/LedgerService.java
+++ src/main/java/com/ledger/LedgerService.java
@@ -8,8 +8,8 @@ public class LedgerService {
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        String filePath= sc.nextLine();
+//        String filePath= sc.nextLine();
         FileProcessor fileProcessor = new FileProcessor();
-        fileProcessor.processFileCommands(filePath);
+        fileProcessor.processFileCommands(args[0]);
     }
 }
