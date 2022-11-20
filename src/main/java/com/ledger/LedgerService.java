bf0b87b8f3a927fc6833525f08df5203534a5d0b
Akash Garg <akgarg023@gmail.com>
2022-11-20 18:12:50 +0530

    removed message from main

--- src/main/java/com/ledger/LedgerService.java
+++ src/main/java/com/ledger/LedgerService.java
@@ -8,7 +8,6 @@ public class LedgerService {
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.print("Enter file path: ");
         String filePath= sc.nextLine();
         FileProcessor fileProcessor = new FileProcessor();
         fileProcessor.processFileCommands(filePath);
